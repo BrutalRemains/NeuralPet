e4ad743f2f54d2620113dcc0c81068f6150d34e2
Kristian Roberts <kristian@localhost.localdomain>
2026-04-18 13:01:25 -0400

    refactor cli loop to check for a loaded creature to see if first time prompt is needed

--- cli.py
+++ cli.py
@@ -1,39 +1,43 @@
+
 from entity.creature import Creature
 from services.llama import get_llm
+from data.database import save_creature, load_creature, row_to_creature
 
 
 def chat():
-    running = True
-    creature = Creature(name=None, species=None)
+    
+    load_creature_data = load_creature()
+    
     # llm = get_llm()
 
-    while running:
-        if creature.name is None:
-            print("Hello! Welcome to NeuroPet!")
-            print("")
-            print("Let's start by creating your creature.")
-            print("First, let's choose a species for your creature.")
-            print("It can be anything you like! The species you choose will influence your creature's behavior")
-            creature.species = input("What species would you like your creature to be? ")
-            while creature.species.strip() == "":
-                creature.species = input("What species would you like your creature to be? ")
-                if creature.species.strip() == "":
-                    continue
-            creature.name = input("What would you like to name your creature? ")
-            while creature.name.strip() == "":
-                creature.name = input("What would you like to name your creature? ")
-                if creature.name.strip() == "":
-                    continue
-                
-            print(f"Great! You've created a {creature.species} named {creature.name}.")
-            print("Now, let's interact with your creature! You can feed it, play with it, or just talk to it.")
-
-        if creature.name is not None:
-            print("")
-            print("Welcome back to your NeuroPet!")
-            print(f"What would you like to do with {creature.name}?")
-            print("You can feed it, play with it, or just talk to it.")
+    
+    if load_creature_data is None:
+        print("Hello! Welcome to NeuroPet!")
+        print("")
+        print("Let's start by creating your creature.")
+        print("First, let's choose a species for your creature.")
+        print("It can be anything you like! The species you choose will influence your creature's behavior")
+        species = input("What species would you like your creature to be? ")
+        while species.strip() == "":
+            species = input("What species would you like your creature to be? ")
+            if species.strip() == "":
+                continue
+        name = input("What would you like to name your creature? ")
+        while name.strip() == "":
+            name = input("What would you like to name your creature? ")
+            if name.strip() == "":
+                continue
             
+        print(f"Great! You've created a {species} named {name}.")
+        print("Now, let's interact with your creature! You can feed it, play with it, or just talk to it.")
+
+    else:
+            creature = row_to_creature(load_creature_data)
+    
+    running = True
+    while running:
+        print("")
+        
     
 
 
